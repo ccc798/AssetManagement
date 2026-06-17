71ed0db554a7b095d5cf65a6322883f3a0e5bad2
ccc798 <rzzzzzzb@qq.com>
2026-06-17 23:28:47 +0800

    fix

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -23,7 +23,7 @@ android {
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
-        versionName = "0.0.14"
+        versionName = "0.0.16"
     }
 
     buildTypes {
@@ -48,6 +48,7 @@ flutter {
 dependencies {
     coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.0.4")
 }
+
 
 
 
